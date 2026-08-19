36e8384fc24f31dca470300d6ce431b2ad48f127
jbspears25 <joshspears250@gmail.com>
2026-08-18 22:48:14 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.REN_CONFIG = {
-  SUPSUPABASE_URL: "https://pqcchmxghkbamqizrnef.supabase.co",
+  SUPABASE_URL: "https://pqcchmxghkbamqizrnef.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_e9BvUe4SOFNQbHioX64kTQ_eyWzV2Wi"
 };
